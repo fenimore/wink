06ca8524febaf93c83bc7bbd89be7382498f212f
polypmer <exorable.ludos@gmail.com>
2017-03-18 22:19:51 -0400

    Javascript that thumbnail

--- gallery.php
+++ gallery.php
@@ -44,44 +44,77 @@ echo '<br>';
 $download = 'zip.php?category='. $category .'&gallery='.$gallery;
 echo '<a href='.$download.' class="nav-control"><span class="glyphicon glyphicon-cloud-download" aria-hidden="true"></span></a>';
 echo '</div>';
-if(null==$gallery or null==$category) {
-    echo 'ERROR: gallery and/or category parameters not specified.';
-} else {
-    $path = 'media/'. $category . '/' . $gallery . '/';
-    $images = glob($path."*.{[jJ][pP][gG],gif,jpeg,svg,bmp,png}", GLOB_BRACE);
-    //z$info = pathinfo($image);
-    $src = 'view.php?category=' . $category . '&gallery=' . $gallery . '&index=';
-    //    $title = str_replace($spaces, " ", $info['filename']);
-    $size = sizeof($images);
-    echo '<div class="col-md-8">';
-    echo '<img src="#" id="image" alt="" width="auto" height="auto" class="img-responsive center-block" >';
-    echo '<br><div class="image-title">' . $title . '</div>';
-    echo '</div>'; // Col
-    // Next Picture
-    echo '<div class="col-md-1 text-right" style="z-index:100">';
-    echo '<a href=# onclick="next()"';
-    echo ' class="nav-control"><span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span></a>';
-    echo '<br><br>';
-    echo '<img src="loading.gif" style="display:none;"; id="loading" alt="" width="auto" height="auto" >';
-    echo '</div>';
+//if(null==$gallery or null==$category) {
+//    echo 'ERROR: gallery and/or category parameters not specified.';
+// }
+$path = 'media/'. $category . '/' . $gallery . '/';
+$images = glob($path."*.{[jJ][pP][gG],gif,jpeg,svg,bmp,png}", GLOB_BRACE);
+$thumbnails = glob($path.'thumbnails/'."*.{[jJ][pP][gG],gif,jpeg,svg,bmp,png}", GLOB_BRACE);
+//z$info = pathinfo($image);
+$src = 'view.php?category=' . $category . '&gallery=' . $gallery . '&index=';
+$title = str_replace($spaces, " ", $info['filename']);
+$size = sizeof($images);
+echo '<div class="col-md-8">';
+echo '<img src="#" id="image" alt="" width="auto" height="auto" class="img-responsive center-block" >';
+echo '<br><div class="image-title">' . $title . '</div>';
+echo '</div>'; // Col
+// Next Picture
+echo '<div class="col-md-1 text-right" style="z-index:100">';
+echo '<a href=# onclick="next()"';
+echo ' class="nav-control"><span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span></a>';
+echo '<br><br>';
+echo '<img src="loading.gif" style="display:none;"; id="loading" alt="" width="auto" height="auto" >';
+echo '  </div>'; // Col
+echo '</div>'; //<!-- Row -->
+echo '<div id="slider" class="row">';
+foreach($thumbnails as $key=>$val) {
+    echo '<div class="col-md-1">';
+    echo '  <img src="'.$val.'" class="thmb" id="thumbnail-'.$key;
+    echo '" alt="" width="auto" height="auto"';
+    echo ' class="img-responsive center-block" >';
+    echo '</div>';// col
 }
+
+echo '</div>'; // Row
 ?>
-  </div> <!-- Row -->
+
 </div><!-- Container -->
 
 <script type="text/javascript">
       var index = 0;
       var size = "<?php echo $size ?>";
+
+$(document).ready(function() {
+    $('#slider img:first-child').addClass('active');
+});
+
+
+
       function next() {
           console.log('next');
           index = checkBounds(index, size, 1);
           getImage();
+          var $active = $('div#slider IMG.active');
+          var $next = $active.next();
+          if($active.is(':last-child')){
+              $next = $('#slider img:first-child');
+          }
+          $next.addClass('active');
+          // Change attribute to zoom
+          $active.removeClass('active');
       }
       function prev() {
           console.log('prev');
           index = checkBounds(index, size, -1);
           getImage();
-
+          var $active = $('div#slider IMG.active');
+          var $prev = $active.prev();
+          if($active.is(':first-child')){
+              $prev = $('#slider img:last-child');
+          }
+          $prev.addClass('active');
+          // do something
+          $active.removeClass('active');
       }
 
 function checkBounds(idx, sze, inc) {
