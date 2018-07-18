6a060d7511bff18d626056e95ac90681fae121bd
Fenimore Love <exorable.ludos@gmail.com>
2018-07-17 22:47:06 -0400

    Redirect authenticate

--- gallery.php
+++ gallery.php
@@ -5,7 +5,7 @@ if(!isset($_SESSION['loggedin'])){
     if ( !empty($_GET['gallery'])) {
         $gallery = $_REQUEST['gallery'];
         $category = $_REQUEST['category'];
-        header("Location:auth/login.php?redirect=../gallery.php?gallery=".$gallery."&category=".$category);
+        header("Location:auth/login.php?redirect=../gallery.php?gallery=" . $gallery . "&category=" . $category);
         die();
     } else {
         header("Location:auth/login.php?redirect=../index.php");
