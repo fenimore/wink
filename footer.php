980ecfcdbf697396a0ad8ebf14fdf16f0c29560f
Fenimore Love <exorable.ludos@gmail.com>
2018-12-03 19:13:13 -0500

    Fix footer paths

--- footer.php
+++ footer.php
@@ -3,21 +3,22 @@
 
   $ini = parse_ini_file('wink.ini');
   $publisher = $ini["publisher"];
-
+  $subdir = $ini["subdir"];
   // absolute href paths
-  $index_href = "/index.php";
-  $logout_href = "/auth/logout.php";
-  $admin_href = "/auth/admin.php";
+  $index_href = "$subdir/index.php";
+  $logout_href = "$subdir/auth/logout.php";
+  $admin_href = "$subdir/auth/admin.php";
+  $about_href = "$subdir/about.php";
 ?>
 <footer class="footer">
   <div class="container">
     <p class="text-muted">
-      <a href="/index.php">Directory</a>
-      | <a href="/about.php"><?php echo $publisher;?></a>
-      | <a href=/auth/admin.php>
+      <a href=<?php echo $index_href; ?>>Directory</a>
+      | <a href=<?php echo $about_href; ?>><?php echo $publisher;?></a>
+      | <a href=<?php echo $admin_href; ?>>
       <span class="glyphicon glyphicon-cloud-upload" aria-hidden="true">
       </span></a>
-      | <a href="/auth/logout.php">Logout</a>
+      | <a href=<?php echo $logout_href; ?> >Logout</a>
     </p>
   </div>
 </footer>
